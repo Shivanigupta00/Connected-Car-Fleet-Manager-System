e01c2bf4299885399df0fc9053aa767288856a26
Shivanigupta00 <shivani04112003.sg@gmail.com>
2025-08-05 16:28:18 +0530

    fixed Bugs

--- class.cpp
+++ class.cpp
@@ -12,109 +12,94 @@ enum RegistrationStatus {
     DECOMMISSIONED
 };
 
-class Telemetry_data{
-protected:
+struct Telemetry_data {
     double latitude;
     double longitude;
     double speed;
     string engine_status;
     double battery_level;
     double odometer;
-    string diagnostics_code;  // if any error
+    string diagnostics_code;
     long long timestamp;
-
-
-public:
-    Telemetry_data(double la, double lo, double s, string eng, double battery,double odometer,string diagn, long long ts ):
-    latitude(la),longitude(lo),speed(s),engine_status(eng),battery_level(battery),odometer(odometer),diagnostics_code(diagn),timestamp(ts){}
-
-    double getspeed() const{
-        return speed;
-    }
-    double getfuel() const{
-        return battery_level;
-    }
-    double getTimestamp() const{
-        return timestamp;
-    }
-
-
 };
 
-class connect{
-private:
+struct Vehicle_data {
     string VIN;
     string manufacture;
+    string model;
     string fleetId;
     string owner;
     RegistrationStatus status;
-
-public:
-    connect(string V, string manu, string fl,string own, RegistrationStatus status):
-    VIN(V), manufacture(manu),fleetId(fl),owner(own),status(status){}
-
-    string getVIN() const{
-        return VIN;
-    }
-
-    string Manufacture()  const{
-        return manufacture;
-    } 
-
-    string fleetId() const{
-        return fleetId;
-    }
-    string getOwner() const{
-        return owner;
-    }
-    RegistrationStatus getstatus() const{
-        return  status;
-
-    }
-
+    vector<Telemetry_data> previous_data;
 };
 
-class Alert {
-private:
-    static int nextId;
+struct Alert {
     int id;
     string VIN;
     string type;
     string details;
     long long timestamp;
-
-public:
-    Alert(const string& V, const string& type, const string& details, long long ts)
-        : id(nextId++), VIN(V), type(type), details(details), timestamp(ts) {}
-
-    int getId() const {
-        return id;
-    }
-
-    string getType() const {
-        return type;
-    }
-    
-    string getVin() const {
-        return VIN;
-    }
 };
 
-class FleetVehicle{
-private:
-    map<string,connect>vehicles;
-    map<string,Alert>fuels;
+map<string ,Vehicle_data>vehicles;
+map<int,Alert>alerts;
 
-    double speed_limit = 100.0;
-    double fuel_limit = 15.0;
-
-    void generateAlert(const string& vin, const Telemetry_data&data){
-        if(data.s>speed_limit){
-            Alert.insert({{Alert::nextId,id}})
-        }
+int id = 1;
+// Create vehicle management all vehicle data
+void generate_vehicle(const string& VIN,const string& manu,const string&mod,const string&fl,const string& own, RegistrationStatus status){
+    if(vehicles.find(VIN) == vehicles.end()){
+        Vehicle_data newV = {VIN,manu,mod,fl,own,status,{}};
+        vehicles[VIN] = newV;
     }
+}
+// list of all vehicle 
+vector<Vehicle_data>list_All_vehicle(){
+    vector<Vehicle_data>allvehicle;
+    for(auto it: vehicles){
+        allvehicle.push_back(it.second);
+    }
+    return allvehicle;
+}
+//query of data
+Vehicle_data*queryVehicle(const string& VIN){
+    auto it = vehicles.find(VIN);
+    if(it != vehicles.end()){
+        return &(it->second);
+    }
+    return {};
+}
 
-};
+void deleteVehicle(const string& VIN){
+    vehicles.erase(VIN);
+}
+
+void generateAlerts(const string& VIN, const Telemetry_data&data){
+    const double speed_limit = 100.0;
+    const double low_fuel = 15.0;
+
+    if(data.speed >speed_limit){
+        alerts[id++] = {id-1,VIN,"Speed ALerts ","speed exceeded 100 km/h",data.timestamp};
+    }
+    if(data.battery_level <low_fuel){
+        alerts[id++]={id-1,VIN,"Fuel is low","it is below 15%",data.timestamp};
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
